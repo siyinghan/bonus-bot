7c3a6dc351150185caf383970ef7c88823977127
siyinghan <50146714+siyinghan@users.noreply.github.com>
2025-01-31 19:57:17 +0100

    v0.0.5 (#5)

--- main.py
+++ main.py
@@ -3,11 +3,12 @@ Get the daily bonus.
 """
 import logging
 
-from get_daily_bonus import get_hifini, get_hai_tang, get_baidu_tb, get_baidu_baike
+from get_daily_bonus import get_hifini, get_hai_tang, get_ourbits, get_baidu_tb, get_baidu_baike
 
 if __name__ == "__main__":
     get_hifini()
-    get_hai_tang()
+    # get_hai_tang()
+    get_ourbits()
     get_baidu_tb()
     get_baidu_baike()
     logging.info("Done!")
