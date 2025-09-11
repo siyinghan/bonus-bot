08fbf1e102b600a55bbea37e4eed146438d9443c
siying <siyinghan@foxmail.com>
2025-09-11 22:19:42 +0200

    v0.0.7

--- main.py
+++ main.py
@@ -3,11 +3,11 @@ Get the daily bonus.
 """
 import logging
 
-from get_daily_bonus import get_hifini, get_hai_tang, get_baidu_tb, get_baidu_baike, get_ttg
+from get_daily_bonus import get_hifini, get_baidu_tb, get_baidu_baike, get_ttg, get_zmpt
 
 if __name__ == "__main__":
     get_hifini()
-    # get_hai_tang()
+    get_zmpt()
     get_ttg()
     get_baidu_tb()
     get_baidu_baike()
